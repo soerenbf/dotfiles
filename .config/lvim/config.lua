e18294ccc6a4c8ab4cbf6f10e59a94d6dda0a363
Søren Bruus Zeppelin <sz@concordium.com>
2022-12-08 08:55:03 +0100

    format on save

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,7 +13,7 @@ lvim.log.level = "warn"
 lvim.reload_config_on_save = true
 lvim.format_on_save = {
   ---@usage boolean: format on save (Default: false)
-  enabled = false,
+  enabled = true,
   ---@usage pattern string pattern used for the autocommand (Default: '*')
   pattern = "*",
   ---@usage timeout number timeout in ms for the format request (Default: 1000)
