822b687b84f60726c5eef6c2e68a30927921ff1e
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-12-05 08:45:45 +0100

    Add fish config to dotfiles

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+set -gx EDITOR nvim
+alias v='nvim'
+
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gca="git commit -am"
+alias gs="git status"
+alias gcl="git clone --recurse-submodules"
+alias gsw="git switch --recurse-submodules"
+alias gsu="git submodule update --init --recursive"
+
+alias nv="nvim"
+alias em="emacs"
+alias lg="lazygit"
+
+alias rustfmt="cargo +nightly-2023-04-01 fmt"
