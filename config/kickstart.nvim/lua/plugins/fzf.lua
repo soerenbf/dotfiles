c2d2d0dccad9ef5df5d0afb429a4dcc46579389d
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-12-05 11:43:53 +0100

    granular fzf windows

--- config/kickstart.nvim/lua/plugins/fzf.lua
+++ config/kickstart.nvim/lua/plugins/fzf.lua
@@ -1,5 +1,5 @@
-local map = function(keys, func, desc)
-  vim.keymap.set('n', keys, func, { desc = 'Fzf: ' .. desc })
+local map = function(keys, func, desc, opts)
+  vim.keymap.set('n', keys, function() func(opts) end, { desc = 'Fzf: ' .. desc })
 end
 
 return {
@@ -10,15 +10,16 @@ return {
     local fzf = require 'fzf-lua'
     fzf.setup(opts)
 
-    map('<leader>fh', fzf.helptags, '[F]ind [H]elp')
+    map('<leader>fh', fzf.helptags, '[F]ind [H]elp', { winopts = { fullscreen = true } })
     map('<leader>fk', fzf.keymaps, '[F]ind [K]eymaps')
-    map('<leader>ff', fzf.files, '[F]ind [F]iles')
+    map('<leader>ff', fzf.files, '[F]ind [F]iles', { winopts = { fullscreen = true } })
     map('<leader>f?', fzf.builtin, '[F]ind Fzf command ("?" for help)')
     map('<leader>fw', fzf.grep_cword, '[F]ind current [W]ord')
-    map('<leader>fg', fzf.live_grep_glob, '[F]ind by [G]rep')
-    map('<leader>f.', fzf.resume, '[F]ind Resume ("." for repeat)')
-    map('<leader>fr', fzf.oldfiles, '[F]ind [R]ecent Files')
-    map('<leader><leader>', fzf.buffers, '[ ] Find existing buffers')
-    map('<leader>/', fzf.blines, '[/] Search in current buffer')
+    map('<leader>fg', fzf.live_grep_glob, '[F]ind by [G]rep', { winopts = { fullscreen = true } })
+    map('<leader>f.', fzf.resume, '[F]ind Resume ("." for repeat)', { winopts = { fullscreen = true } })
+    map('<leader>fr', fzf.oldfiles, '[F]ind [R]ecent Files', { winopts = { fullscreen = true } })
+    map('<leader><leader>', fzf.buffers, '[ ] Find existing buffers', { winopts = { fullscreen = true } })
+    map('<leader>/', fzf.blines, '[/] Search in current buffer',
+      { winopts = { preview = { hidden = 'hidden' }, width = 120, height = 30 } })
   end
 }
