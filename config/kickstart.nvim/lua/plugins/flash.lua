3ac528ffc09b145db21166cb358e22be1199331b
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-04-02 20:42:56 +0200

    Changed conflicting keybind

--- config/kickstart.nvim/lua/plugins/flash.lua
+++ config/kickstart.nvim/lua/plugins/flash.lua
@@ -10,7 +10,7 @@ return {
   config = true,
   -- stylua: ignore
   keys = {
-    { "<C-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "<m-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
     { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
     { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
     { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
