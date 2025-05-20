ca044e558dabdce088d445fc6aa4fece574cd538
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-05-20 09:48:25 +0200

    Minor fish tweaks

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,7 +4,9 @@ set -x KANATA_TRAY_CONFIG_DIR ~/.config/kanata-tray
 set -x KANATA_TRAY_LOG_DIR ~/.config/kanata-tray
 
 # PATH
-set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
+fish_add_path ~/.local/bin
+fish_add_path /usr/local/go/bin
+fish_add_path $GOPATH/bin
 
 # enable vi mode
 set -g fish_key_bindings fish_vi_key_bindings
@@ -35,27 +37,28 @@ end
 
 if status is-interactive
     if type -q zellij
+        # Helper function to set Zellij tab name
+        function set_zellij_tab_name
+            set -l cmd_line (string split " " -- $argv)
+            set -l process_name $cmd_line[1]
+
+            if test "$process_name" = "v"
+                set process_name (string join " " "nvim" (prompt_pwd))
+            end
+
+            command nohup zellij action rename-tab $process_name >/dev/null 2>&1
+        end
+
         # Update the zellij tab name with the current process name or pwd.
         function zellij_tab_name_update_pre --on-event fish_preexec
             if set -q ZELLIJ
-                set -l cmd_line (string split " " -- $argv)
-                set -l process_name $cmd_line[1]
-                if test -n "$process_name" -a "$process_name" != "z" -a "$process_name" != "cd"
-                    if test "$process_name" = "v"
-                        set process_name (string join " " "nvim" (prompt_pwd))
-                    end
-                    command nohup zellij action rename-tab $process_name >/dev/null 2>&1
-                end
+                set_zellij_tab_name
             end
         end
 
         function zellij_tab_name_update_post --on-event fish_postexec
             if set -q ZELLIJ
-                set -l cmd_line (string split " " -- $argv)
-                set -l process_name $cmd_line[1]
-                if test "$process_name" = "z" -o "$process_name" = "cd" -o "$process_name" = "v"
-                    command nohup zellij action rename-tab (prompt_pwd) >/dev/null 2>&1
-                end
+                command nohup zellij action rename-tab (prompt_pwd) >/dev/null 2>&1
             end
         end
     end
