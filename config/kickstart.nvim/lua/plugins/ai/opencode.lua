74dc65f7393943c91478cdf44572103e6694edcd
Søren Bruus Zeppelin <soerenbf@gmail.com>
2026-01-12 10:56:33 +0100

    Update opencode

--- config/kickstart.nvim/lua/plugins/ai/opencode.lua
+++ config/kickstart.nvim/lua/plugins/ai/opencode.lua
@@ -1,17 +1,25 @@
 return {
   'NickvanDyke/opencode.nvim',
-  dependencies = { 'folke/snacks.nvim' },
-  ---@type opencode.Config
-  opts = {
-    -- Your configuration, if any
+  dependencies = {
+    -- Recommended for `ask()` and `select()`.
+    -- Required for `snacks` provider.
+    ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+    { 'folke/snacks.nvim', opts = { input = {}, picker = {}, terminal = {} } },
   },
+  config = function()
+    ---@type opencode.Opts
+    vim.g.opencode_opts = {
+      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+    }
+    -- Required for `opts.events.reload`.
+    vim.o.autoread = true
+  end,
   -- stylua: ignore
   keys = {
     { '<leader>at', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', },
-    { '<leader>aa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = 'n', },
-    { '<leader>aa', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
-    { '<leader>ap', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
-    { '<leader>an', function() require('opencode').command('session_new') end, desc = 'New session', },
-    { '<leader>ay', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
+    { '<leader>aa', function() require('opencode').ask('@this: ', { submit = true }) end, desc = 'Ask opencode', mode = { 'n', 'v', }, },
+    { '<leader>ab', function() require('opencode').ask('@buffer: ', { submit = true }) end, desc = 'Ask opencode about the current buffer', mode = 'n', },
+    { '<leader>ap', function() require('opencode').select() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
+    { '<leader>an', function() require('opencode').command('session.new') end, desc = 'New session', },
   },
 }
