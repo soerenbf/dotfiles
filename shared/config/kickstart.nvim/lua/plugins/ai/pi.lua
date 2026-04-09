375c4b7f73611447d45e997cb4995e26f490a9d8
Søren Bruus Zeppelin <soerenbf@gmail.com>
2026-04-09 16:15:36 +0200

    Add pi (AI agent) configuration and integration into neovim

---
+++ shared/config/kickstart.nvim/lua/plugins/ai/pi.lua
@@ -0,0 +1,11 @@
+return {
+  'carderne/pi-nvim',
+  config = true,
+  -- stylua: ignore
+  keys = {
+    { '<leader>aa', '<cmd>Pi<cr>', desc = 'Ask pi', mode = { 'n', 'v' }, },
+    { '<leader>ab', '<cmd>PiSendBuffer<cr>', desc = 'Ask pi about the current buffer', mode = 'n', },
+    { '<leader>ap', '<cmd>PiSend<cr>', desc = 'Send prompt to pi', mode = { 'n', 'v' }, },
+    { '<leader>an', '<cmd>PiSessions<cr>', desc = 'Switch pi session', },
+  },
+}
